5d02489085d9c94db8fa6355c3ec3ad19ffa222c
RofieSagaraAMK <rofie8030@students.amikom.ac.id>
2020-12-19 06:28:15 +0700

    Update

--- google.py
+++ google.py
@@ -1,9 +1,23 @@
+import random
 import time
-
 import requests as req
 from selenium import webdriver
 import re
 from bs4 import BeautifulSoup
+from selenium.common.exceptions import NoSuchElementException
+
+
+def get_random_scroll_script():
+    rand = random.randint(-500, 1000)
+    script = 'window.scrollBy(0,'+str(rand)+')'
+    print('Get scroll random for the pixel ', rand, ' to emulate person with script ', script)
+    return script
+
+
+def random_sleep():
+    rand = random.randint(3, 9)
+    print('Do sleep for ', rand, ' seconds to emulate person')
+    time.sleep(rand)
 
 
 def main():
@@ -33,10 +47,24 @@ def main():
                     print('AttributeError check the website probably unique or website already change the view.')
                     is_auto = False
             print('Try to make next result')
-            browser.find_element_by_id("pnnext").click()
-            time.sleep(5)
+            try:
+                # Emulate the person like scrolling before press next button
+                button_next = browser.find_element_by_id('pnnext')
+                random_sleep()
+                random_step = random.randint(3, 10)
+                print('Emulate random step for scrolling')
+                for i in range(random_step):
+                    random_sleep()
+                    browser.execute_script(get_random_scroll_script())
+                print('Scrolling to end of page')
+                browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+                print('Click next button')
+                button_next.click()
+                is_auto = True
+            except NoSuchElementException:
+                print('Seems like google detects our is robot. please fill the robot field')
+                is_auto = False
             f.close()
-            is_auto = True
         elif value == 'c':
             print('Close the program!')
             break
